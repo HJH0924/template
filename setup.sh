71985dc43db70e4ccdd4a554bccdaabb45eae771
Tvux <1165411520@qq.com>
2024-12-07 11:29:31 +0800

    feat: download go-gitlint

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ target_path="$1"
 cp ./Makefile ${target_path}
 cp ./gitignore ${target_path}/.gitignore
 cp ./golangci.yaml ${target_path}/.golangci.yaml
+cp ./go-gitlint $(HOME)/go/bin/
 cp -r ./githooks ${target_path}
